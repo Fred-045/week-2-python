82c6445a85e901eefd2e8cee4a41b0a31dd388a0
Fred-045 <mwangifrederick773@gmail.com>
2024-08-15 19:15:42 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,18 @@
+
+
+# creating an empty list
+my_list=[]
+# Appending the elements
+my_list.extend([10,20,30,40])
+# Inserting elements 15
+my_list.insert(1, 15)
+#extending the list
+my_list.extend([50, 60, 70])
+# removing  the elemntnt
+my_list.pop()
+#sorting accounding ti ascending order
+my_list.sort()
+
+
+print(my_list.index(30))
+print(my_list)
